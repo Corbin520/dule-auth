fc04ecaf40b482ea774d2e8a598f34df261d3758
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2020-10-23 17:06:46 -0600

    added things

--- Dule-Auth/app.js
+++ Dule-Auth/app.js
@@ -1,19 +1,22 @@
 var htmlAuth = document.getElementById("authCode");
 var usernameField = document.getElementById("username");
 var passwordField = document.getElementById("password");
+document.getElementById("duleAuth-input").style.visibility = "hidden";
 var response = "";
 
 
+
 function varificationCode() {
-    // passwordField.hide()
-    // usernameField.hide()
+    document.getElementById("user").style.visibility = "hidden";
+    document.getElementById("pass").style.visibility = "hidden";
+    document.getElementById("duleAuth-input").style.visibility = "block"; // need to get it to show
+    document.getElementById("login-bttn").style.visibility = "hidden";
     var numberArray = ["2", "1", "8", "9", "6", "5", "7", "3", "4", "0"];
     var response = "";
     for(var i = 0; i < numberArray.length; i++) {
         var random = Math.floor(Math.random() * numberArray[i])
         var randomAnswer = numberArray[random]
         response += randomAnswer
-    }
-    console.log(response)  
+    } 
     htmlAuth.textContent = "Verification Code: " + response
 }
